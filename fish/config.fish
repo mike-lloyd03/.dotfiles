e052f8775a6f531a03676d9893bb233f9c6143e8
Mike Lloyd <mike.lloyd@teradata.com>
2024-01-05 12:48:36 -0800

    Remove KUBECONFIG variable

--- fish/config.fish
+++ fish/config.fish
@@ -21,11 +21,11 @@ set -U fish_greeting ""
 if [ "$(uname -n)" = "TD-C02FK3H8MD6T" ]
     fish_add_path "/usr/local/opt/gnubin:/usr/local/bin"
     # set -x MANPATH /usr/local/opt/gnuman:${MANPATH:-/usr/share/man}
-    set -x KUBECONFIG "$HOME/.kube/config:$HOME/.kube/config-dev"
+    # set -x KUBECONFIG "$HOME/.kube/config:$HOME/.kube/config-dev"
 end
 
 if [ "$(uname -n)" = dev ]
-    set -x KUBECONFIG "$HOME/.kube/config:$HOME/.kube/config-dev"
+    # set -x KUBECONFIG "$HOME/.kube/config:$HOME/.kube/config-dev"
 end
 
 # Keybinds
