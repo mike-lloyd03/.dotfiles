ea11e0de5224e11df057e2b45a282b37d847d639
Mike Lloyd <mike.lloyd@teradata.com>
2024-01-19 14:24:18 -0800

    Update wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,12 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 local config = {}
 
-config.font = wezterm.font 'JetBrainsMonoNL Nerd Font Mono'
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_wayland = true
 config.enable_scroll_bar = false
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.window_padding = {
     left = 0,
     right = 0,
@@ -14,21 +14,20 @@ config.window_padding = {
     bottom = 0,
 }
 config.window_background_gradient = {
-    orientation = 'Vertical',
+    orientation = "Vertical",
     colors = {
-        '#1a1c23',
-        '#343434',
-        '#1a1c23',
+        "#1a1c23",
+        "#343434",
+        "#1a1c23",
     },
 }
 
-config.color_scheme = 'Catppuccin Mocha'
-
-config.front_end = "Software"
+config.color_scheme = "One Dark (Gogh)"
 
 config.keys = {
-    {key = 'c', mods = 'CMD|SHIFT', action = act.CopyTo 'Clipboard'},
-    {key = 'v', mods = 'CMD|SHIFT', action = act.PasteFrom 'Clipboard'},
+    { key = "c", mods = "CMD|SHIFT", action = act.CopyTo("Clipboard") },
+    { key = "v", mods = "CMD|SHIFT", action = act.PasteFrom("Clipboard") },
+    { key = "w", mods = "CMD|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
 }
 
 return config
