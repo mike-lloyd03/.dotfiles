eeddf381f5dc63b69d11ddda23926464f7b3fc1b
Mike Lloyd <mike.lloyd03@pm.me>
2024-04-24 14:45:48 -0700

    rust-tools rustacean

--- nvim/lua/plugs/language_support.lua
+++ nvim/lua/plugs/language_support.lua
@@ -4,50 +4,9 @@ return {
         ft = "csv",
     },
     {
-        "simrat39/rust-tools.nvim",
-        -- ft = {
-        --     "rust",
-        --     "toml",
-        -- },
-        opts = {
-
-            tools = {
-                inlay_hints = {
-                    only_current_line = true,
-                    show_parameter_hints = false,
-                    -- highlight = "VertSplit",
-                    -- highlight = "lualine_a_mode_inactive",
-                    highlight = "DevIconDoc",
-                },
-            },
-            server = {
-                on_attach = function(client, bufnr)
-                    -- Hover actions
-                    vim.keymap.set(
-                        "n",
-                        "<C-space>",
-                        require("rust-tools").hover_actions.hover_actions,
-                        { buffer = bufnr }
-                    )
-                    -- Code action groups
-                    vim.keymap.set(
-                        "n",
-                        "<Leader>a",
-                        require("rust-tools").code_action_group.code_action_group,
-                        { buffer = bufnr }
-                    )
-                    require("nvim-navic").attach(client, bufnr)
-                end,
-                settings = {
-                    ["rust-analyzer"] = {
-                        checkOnSave = {
-                            command = "clippy",
-                        },
-                        inlayHints = { locationLinks = false },
-                    },
-                },
-            },
-        },
+        "mrcjkb/rustaceanvim",
+        version = "^4",
+        lazy = false,
     },
     {
         "ron-rs/ron.vim",
