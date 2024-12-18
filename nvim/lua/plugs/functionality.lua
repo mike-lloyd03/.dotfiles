6f4bb1f3f0c4157d05a52a43b79b8c9223a9b852
Mike Lloyd <mike.lloyd03@pm.me>
2024-12-18 07:46:18 -0800

    Add grug-far

--- nvim/lua/plugs/functionality.lua
+++ nvim/lua/plugs/functionality.lua
@@ -159,7 +159,7 @@ return {
         opts = {
             label = { uppercase = false },
             modes = {
-                search = { enabled = true },
+                search = { enabled = false },
                 char = { enabled = false },
             },
         },
@@ -252,5 +252,27 @@ return {
     {
         "windwp/nvim-ts-autotag",
         opts = {},
+    },
+    {
+        "MagicDuck/grug-far.nvim",
+        opts = { headerMaxWidth = 80 },
+        cmd = "GrugFar",
+        keys = {
+            {
+                "<space>S",
+                function()
+                    local grug = require("grug-far")
+                    local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+                    grug.open({
+                        transient = true,
+                        prefills = {
+                            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+                        },
+                    })
+                end,
+                mode = { "n", "v" },
+                desc = "Search and Replace",
+            },
+        },
     },
 }
