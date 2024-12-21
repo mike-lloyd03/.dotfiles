631c5cf221de5a0ee7ccfdf0aa133d4ee44f923a
Mike Lloyd <mike.lloyd03@pm.me>
2024-12-20 19:46:27 -0800

    Switch out nvim-cmp for blink

--- nvim/lua/plugs/lsp.lua
+++ nvim/lua/plugs/lsp.lua
@@ -81,117 +81,50 @@ return {
         end,
     },
     {
-        "hrsh7th/nvim-cmp",
-        -- "yioneko/nvim-cmp",
-        -- branch = "perf",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "onsails/lspkind-nvim",
-            {
-                "L3MON4D3/LuaSnip",
-                build = "make install_jsregexp",
-                dependencies = {
-                    "saadparwaiz1/cmp_luasnip",
-                    {
-                        "rafamadriz/friendly-snippets",
-                        config = function()
-                            require("luasnip.loaders.from_vscode").lazy_load()
-                        end,
-                    },
-                    "golang/vscode-go",
-                    "rust-lang/vscode-rust",
-                },
-            },
-        },
-        config = function()
-            local cmp = require("cmp")
-            local lspkind = require("lspkind")
-            local luasnip = require("luasnip")
-
-            cmp.setup({
-                -- Enable LSP snippets
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
+        "saghen/blink.cmp",
+        dependencies = "rafamadriz/friendly-snippets",
+        version = "*",
+        opts = {
+            keymap = {
+                preset = "enter",
+                ["<Tab>"] = {
+                    function(cmp)
+                        if cmp.snippet_active() then
+                            return cmp.snippet_forward()
+                        else
+                            return cmp.select_next()
+                        end
                     end,
+                    "fallback",
                 },
-                mapping = {
-                    ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                    ["<Up>"] = cmp.mapping.select_prev_item(),
-                    ["<Down>"] = cmp.mapping.select_next_item(),
-                    ["<C-j>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.ConfirmBehavior.Insert,
-                        select = false,
-                    }),
+                ["<S-Tab>"] = {
+                    function(cmp)
+                        if cmp.snippet_active() then
+                            return cmp.snippet_backward()
+                        else
+                            return cmp.select_prev()
+                        end
+                    end,
+                    "fallback",
                 },
-
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "symbol_text",
-                        maxwidth = 50,
-                        -- menu = {
-                        --     nvim_lsp = "[LSP]",
-                        --     luasnip = "[LuaSnip]",
-                        --     path = "[Path]",
-                        --     buffer = "[Buffer]",
-                        -- },
-                    }),
-                },
-
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "path" },
-                }, {
-                    { name = "buffer" },
-                }),
-            })
-        end,
-        keys = {
-            {
-                "<Tab>",
-                function()
-                    local luasnip = require("luasnip")
-                    local cmp = require("cmp")
-
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.locally_jumpable(1) then
-                        luasnip.jump(1)
-                    else
-                        return "<Tab>"
-                    end
-                end,
-                expr = true,
-                silent = true,
-                mode = { "i", "s" },
+                ["<C-k>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-j>"] = { "scroll_documentation_down", "fallback" },
             },
-            {
-                "<S-Tab>",
-                function()
-                    local luasnip = require("luasnip")
-                    local cmp = require("cmp")
 
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        return "<S-Tab>"
-                    end
-                end,
-                expr = true,
-                silent = true,
-                mode = { "i", "s" },
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = "mono",
+            },
+
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+                cmdline = {},
+            },
+
+            completion = {
+                documentation = { auto_show = true, auto_show_delay_ms = 500 },
             },
         },
+        opts_extend = { "sources.default" },
     },
 }
