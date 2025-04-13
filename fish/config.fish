95e0d2bc470ec54509f9b60d0730552d187fa5c8
Mike Lloyd <mike.lloyd03@pm.me>
2025-04-12 20:54:06 -0700

    Fix cursor blinking

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,10 @@ source "$HOME/.config/fish/aliases.fish"
 # vim bindings
 fish_vi_key_bindings
 set fish_vi_force_cursor true
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
+set fish_cursor_default block blink
+set fish_cursor_insert line blink
+set fish_cursor_replace_one underscore blink
+set fish_cursor_visual block blink
 
 set -Ux PAGER less
 set -Ux EDITOR nvim
