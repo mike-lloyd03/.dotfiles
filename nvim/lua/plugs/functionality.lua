e57beaf72e31151515e811053f2a0790fc0d6070
Mike Lloyd <mike.lloyd03@pm.me>
2025-08-27 13:38:57 -0700

    Add formatting toggle

--- nvim/lua/plugs/functionality.lua
+++ nvim/lua/plugs/functionality.lua
@@ -19,9 +19,16 @@ return {
                 async = false,
                 quiet = false,
             },
-            format_on_save = {
-                lsp_format = "fallback",
-            },
+
+            -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+            format_on_save = function(bufnr)
+                -- Disable with a global or buffer-local variable
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { timeout_ms = 500, lsp_format = "fallback" }
+            end,
+
             formatters = {
                 stylua = {
                     prepend_args = { "--indent-type", "spaces" },
@@ -56,6 +63,7 @@ return {
                 zsh = { "shfmt" },
             },
         },
+
         init = function()
             vim.api.nvim_create_user_command("Format", function(args)
                 local range = nil
@@ -68,7 +76,34 @@ return {
                 end
                 require("conform").format({ async = true, lsp_format = "fallback", range = range })
             end, { range = true })
+
+            vim.api.nvim_create_user_command("FormatDisable", function(args)
+                if args.bang then
+                    -- FormatDisable! will disable formatting just for this buffer
+                    vim.b.disable_autoformat = true
+                else
+                    vim.g.disable_autoformat = true
+                end
+            end, {
+                desc = "Disable autoformat-on-save",
+                bang = true,
+            })
+
+            vim.api.nvim_create_user_command("FormatEnable", function()
+                vim.b.disable_autoformat = false
+                vim.g.disable_autoformat = false
+            end, {
+                desc = "Re-enable autoformat-on-save",
+            })
+
+            vim.api.nvim_create_user_command("FormatToggle", function()
+                vim.b.disable_autoformat = not vim.b.disable_autoformat
+                vim.g.disable_autoformat = not vim.g.disable_autoformat
+            end, {
+                desc = "Toggle autoformat-on-save",
+            })
         end,
+
         keys = {
             {
                 "<C-f>",
